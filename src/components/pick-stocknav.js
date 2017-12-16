282a9aa820f417bfe4db997e192b16011920834d
Manoj_RG <manojramcbe@gmail.com>
2017-12-17 05:01:27 +0530

    Added disable funnctionality to the Prev and Next buttons

--- src/components/pick-stocknav.js
+++ src/components/pick-stocknav.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class pickStockNav extends Component {
     render() {
         return  (    
-            <a className="stock-nav-btn" onClick={this.props.countUpdateHandler.bind(this, this.props.counter)}> {this.props.text} </a>
+            <a className="stock-nav-btn" disabled={this.props.disableOn} onClick={this.props.countUpdateHandler.bind(this, this.props.counter)}> {this.props.text} </a>
         );
     }
 }
